a7772d8bd30d8c40c6050bb8217c46e757393f05
Richard Parayno <broodheart@gmail.com>
2024-09-12 22:14:12 +0800

    updated mason packages

--- init.lua
+++ init.lua
@@ -604,6 +604,9 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'prettierd', -- Prettierd formatter
+        'eslint-lsp', -- ESLint LSP
+        'typescript-language-server', -- typescript language server
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
