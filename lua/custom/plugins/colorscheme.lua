39bd6e06b003995ae50667027698c432533bb750
Richard Parayno <richard-parayno@users.noreply.github.com>
2025-08-04 22:43:50 -0700

    update colors

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -38,9 +38,31 @@ return {
           VertSplit = { bg = 'None' },
         },
       }
-
-      vim.cmd.colorscheme 'ayu-mirage'
-      vim.cmd.hi 'Comment gui=none'
+    end,
+  },
+  {
+    'Everblush/nvim',
+    name = 'everblush',
+    config = function()
+      require('everblush').setup {}
+    end,
+  },
+  {
+    'zenbones-theme/zenbones.nvim',
+    -- Optionally install Lush. Allows for more configuration or extending the colorscheme
+    -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
+    -- In Vim, compat mode is turned on as Lush only works in Neovim.
+    dependencies = 'rktjmp/lush.nvim',
+    lazy = false,
+    priority = 1000,
+    -- you can set set configuration options here
+    config = function()
+      vim.g.neobones_darken_comments = 45
+      vim.g.neobones_transparent_background = true
+      vim.g.neobones_solid_vert_split = true
+      -- vim.g.neobones_solid_line_nr = true
+      vim.g.neobones_solid_float_border = true
+      vim.cmd.colorscheme 'neobones'
     end,
   },
 }
