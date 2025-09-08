ab1da332143c952eb95842720e950ef67c17672c
Richard Parayno <richard-parayno@users.noreply.github.com>
2025-09-08 13:39:39 +0800

    update

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -24,7 +24,7 @@ return {
         offsets = {},
         show_tab_indicators = false,
       },
-      highlights = require('catppuccin.groups.integrations.bufferline').get(),
+      highlights = require('catppuccin.groups.integrations.bufferline').get_theme(),
     }
   end,
 }
