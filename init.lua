3f53127b0e84c27456107aa935d4ab0331a40434
Richard Parayno <richard-parayno@users.noreply.github.com>
2025-10-28 18:46:59 +0800

    update

--- init.lua
+++ init.lua
@@ -585,17 +585,6 @@ require('lazy').setup({
       -- See :h blink-cmp-config-keymap for defining your own keymap
       keymap = {
         preset = 'default',
-        -- For Sidekick.nvim:
-        ['<Tab>'] = {
-          'snippet_forward',
-          function() -- sidekick next edit suggestion
-            return require('sidekick').nes_jump_or_apply()
-          end,
-          function() -- if you are using Neovim's native inline completions
-            return vim.lsp.inline_completion.get()
-          end,
-          'fallback',
-        },
       },
 
       appearance = {
